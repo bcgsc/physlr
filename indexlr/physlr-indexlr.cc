04aa3b77b30210451adb5100d436520b29eb2140
spalchowdhury <spchowdhury@bcgsc.ca>
2018-12-20 17:30:03 -0800

    Change algorithm in getMinimizers(). Change I/O loop. Improve style.

--- indexlr/physlr-indexlr.cc
+++ indexlr/physlr-indexlr.cc
@@ -31,130 +31,90 @@ static vector<uint64_t> hashKmers(const string &readstr, const size_t k) {
 	return hashes;
 }
 
-// Find the minimum hash values in a sliding window of size w.
-//
-// A window of hash values has one new hash value with respect to the previous
-// window (and one less). If the new hash value is less than or equal to the
-// minimum of the previous window, it is also the minimum of the current
-// window. The if statement within the outer for loop checks for this condition
-// and skips the inner for loop which finds the minimum in the current window.
+// Find all the minimizers from a list of hash values. A minimizer is the
+// minimum hash value in a window of size w sliding along that list.
 static vector<uint64_t> getMinimizers(const vector<uint64_t> &hashes,
 				      const size_t w) {
-	vector<uint64_t> minSet;
-	minSet.reserve(w);
-	uint64_t oldMin = 0;
-	for (size_t i = 0; i < hashes.size() - w + 1; ++i) {
-		uint64_t newMin = hashes[i + w - 1];
-		if (newMin > oldMin) {
-			for (size_t j = i; j < i + w - 1; ++j) {
-				if (hashes[j] < newMin)
-					newMin = hashes[j];
-			}
+	vector<uint64_t> minimizerSet;
+	minimizerSet.reserve(w);
+	int prevIndex = -1;
+	vector<uint64_t>::const_iterator windowIter = hashes.begin();
+	for (unsigned i = 1; i < hashes.size() - w + 1; ++i, ++windowIter) {
+		vector<uint64_t>::const_iterator min =
+			std::min_element(windowIter, windowIter + w);
+		int minIndex = std::distance(windowIter, min);
+		minIndex += i;
+		if (minIndex > prevIndex) {
+			prevIndex = minIndex;
+			minimizerSet.push_back(*min);
 		}
-		vector<uint64_t>::iterator iter =
-			std::lower_bound(minSet.begin(), minSet.end(), newMin);
-		if (iter == minSet.end() || newMin < *iter)
-			minSet.insert(iter, newMin);
-		oldMin = newMin;
 	}
-	return minSet;
+	return minimizerSet;
 }
 
 // Print the barcode and minimzers of a read
 static void printMinimizedRead(const string &barcode,
-			       vector<uint64_t> &minSet) {
+			       vector<uint64_t> &minimizerSet) {
 	cout << barcode << "\t";
-	for (vector<uint64_t>::iterator iter = minSet.begin();
-	     iter != minSet.end(); ++iter)
-		cout << *iter << " ";
+	for (auto &m : minimizerSet)
+		cout << m << " ";
 	cout << "\n";
 }
 
 // Read a FASTQ file and reduce each read to a set of minimizers
-static void minimizeReads(std::istream *is, const size_t k, const size_t w,
-			  bool verbose) {
-	size_t nread = 0, nline = 0, readlen;
-	bool io_error = false;
-	while (!is->eof() && !io_error) {
-		bool barcode_error = false;
-		string line, comment, barcode;
-		for (int i = 1; i <= 4; ++i) {
-			if (!getline(*is, line)) {
-				io_error = true;
-				break;
-			}
-			++nline;
-			if (i == 1) {
-				istringstream iss(line);
-				iss >> comment >> barcode;
-				string prefix = barcode.substr(0, 5);
-				if (prefix != "BX:Z:") {
-					if (verbose) {
-						cerr << "WARN: Unknown "
-						     << "barcode prefix '"
-						     << prefix
-						     << "' on line " << nline
-						     << "\n";
-					}
-					barcode_error = true;
-					continue;
-				}
-				barcode = barcode.substr(5);
-			}
-			else if (i == 2) {
-				++nread;
-				if (barcode_error) {
-					if (verbose) {
-						cerr << "WARN: Skip read "
-						     << nread
-						     << " on line " << nline
-						     << "; malformed barcode "
-						     << "on line " << nline - 1
-						     << "\n";
-					}
-					continue;
-				}
-				readlen = line.size();
-				if (k > readlen) {
-					if (verbose) {
-						cerr << "WARN: Skip read "
-						     << nread
-						     << " on line " << nline
-						     << "; k > read length "
-						     << "(k = " << k
-						     << ", read length = "
-						     << readlen << ")\n";
-					}
-					continue;
-				}
-				vector<uint64_t> hashes = hashKmers(line, k);
-				// NOTE: The predicate P(#kmers != #hashes) will
-				// be true when reads contains Ns, so check with
-				// the number of hashes ntHash gives after
-				// hashing a read. ntHash takes care to skip Ns.
-				if (w > hashes.size()) {
-					if (verbose) {
-						cerr << "WARN: Skip read "
-						     << nread
-						     << " on line " << nline
-						     << "; window size > #hashes"
-						     << "(w = " << w
-						     << ", #hashes = "
-						     << hashes.size() << ")\n";
-					}
-					continue;
-				}
-				vector<uint64_t> minSet =
-					getMinimizers(hashes, w);
-				printMinimizedRead(barcode, minSet);
-				hashes.clear();
-				minSet.clear();
-			}
+static void minimizeReads(std::istream *is, const size_t k, const size_t w, bool verbose) {
+	size_t nread = 0, nline = 0;
+	string sequence, header, comment, barcode;
+	while (getline(*is, header)
+	       && getline(*is, sequence)
+	       && (*is).ignore(std::numeric_limits<std::streamsize>::max(), '\n')
+	       && (*is).ignore(std::numeric_limits<std::streamsize>::max(), '\n')) {
+		// Ignore the next two lines; '+' and quality string.
+		nline += 4;
+		nread += 1;
+		// Extract the barcode.
+		istringstream iss(header);
+		iss >> comment >> barcode;
+		string prefix = barcode.substr(0, 5);
+		if (prefix != "BX:Z:") {
+			std::cerr << "physlr-indexlr: error: expected BX:Z: and saw "
+				  << prefix << " at line " << nline - 3 << "\n";
+			exit(EXIT_FAILURE);
 		}
+		barcode = barcode.substr(5);
+		// Validate parameters.
+		if (k > sequence.size()) {
+			if (verbose) {
+				std::cerr << "warning: skip read " << nread
+					  << " on line " << nline - 2
+					  << "; k > read length " << "(k = " << k
+					  << ", read length = " << sequence.size() << ")\n";
+			}
+			continue;
+		}
+		// Hash the kmers.
+		vector<uint64_t> hashes = hashKmers(sequence, k);
+		// NOTE: The predicate P(#kmers != #hashes) will be true when
+		// reads contains Ns, so check with the number of hashes ntHash
+		// gives after hashing a read. ntHash takes care to skip Ns.
+		if (w > hashes.size()) {
+			if (verbose) {
+				std::cerr << "warning: skip read " << nread
+					  << " on line " << nline - 2
+					  << "; window size > #hashes (w = " << w
+					  << ", #hashes = " << hashes.size() << ")\n";
+			}
+			continue;
+		}
+		// Minimerize.
+		vector<uint64_t> minimizerSet = getMinimizers(hashes, w);
+		printMinimizedRead(barcode, minimizerSet);
+		hashes.clear();
+		minimizerSet.clear();
 	}
 }
 
-void printUsage(const string &progname) {
+static void printUsage(const string &progname) {
 	cout << "Usage:  " << progname <<
 		"  -k K -w W [-v] file...\n\n"
 		"  -k K     use K as k-mer size\n"
@@ -164,7 +124,7 @@ void printUsage(const string &progname) {
 		"  file     space separated list of FASTQ files\n";
 }
 
-void printErrorMsg(const string &progname, const string &msg) {
+static void printErrorMsg(const string &progname, const string &msg) {
 	cerr << progname << ": " << msg << "\n"
 		"Try 'physlr-indexlr --help' for more information.\n";
 }
@@ -180,7 +140,7 @@ int main(int argc, char *argv[]) {
 	bool failed  = false;
 	bool w_set   = false;
 	bool k_set   = false;
-	struct option longopts[]= {
+	static const struct option longopts[]= {
 				   {"help", no_argument, &help, 1},
 				   {0, 0, 0, 0}
 	};
@@ -204,10 +164,7 @@ int main(int argc, char *argv[]) {
 			exit(EXIT_FAILURE);
 		}
 	}
-	vector<string> infile;
-	infile.reserve(argc - optind);
-	for (int i = optind; i < argc; ++i)
-		infile.push_back(argv[i]);
+	std::vector<string> infile(&argv[optind], &argv[argc]);
 	if (argc < 2) {
 		printUsage(progname);
 		exit(EXIT_FAILURE);
@@ -240,11 +197,11 @@ int main(int argc, char *argv[]) {
 	}
 	if (failed)
 		exit(EXIT_FAILURE);
-	for (unsigned i = 0; i < infile.size(); ++i) {
-		ifstream ifs(infile[i]);
+	for (auto &f : infile) {
+		ifstream ifs(f);
 		if (!ifs) {
-			cerr << "ERROR: Failed to open: " << infile[i] << "\n";
-			continue;
+			std::cerr << "phslyr-indexlr: error: failed to open: " << f << "\n";
+			exit(EXIT_FAILURE);
 		}
 		minimizeReads(&ifs, k, w, verbose);
 	}
