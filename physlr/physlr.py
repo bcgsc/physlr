40319fb6e1895b85275d086131e87a989ed94c89
afshinfard <a.afshinfard@gmail.com>
2019-05-27 14:44:15 -0700

    bug fix + strategy change

--- physlr/physlr.py
+++ physlr/physlr.py
@@ -1526,7 +1526,7 @@ class Physlr:
         communities = [g[u].keys()]
         communities_final = []
 
-        for algorithm in ["bc", "k3", "bc", "cos", "bc", "sqCos", "bc"]:
+        for algorithm in ["bc", "k3", "cos", "sqCos"]:
             communities_final.clear()
             if algoritm == "bc":
                 for component in communities:
