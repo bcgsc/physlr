3f590e72d82b8c7aebde14a9b7a9b4c05aed9db6
Justin Chu <JustinChu1989@gmail.com>
2019-06-12 15:41:16 -0700

    Update src/physlr-overlap.cc
    Co-Authored-By: Shaun Jackman <sjackman@gmail.com>

--- src/physlr-overlap.cc
+++ src/physlr-overlap.cc
@@ -52,7 +52,6 @@ printHelpDialog()
 	    "Usage: physlr-overlap [OPTION]... [MINIZERS.tsv]\n"
 	    "Read a sketch of linked reads and find overlapping barcodes.\n"
 	    "  -n, --min-n=INT   Remove edges with fewer than n shared markers [0].\n"
-		//"  -t, --threads=INT Number of threads [1].\n"
 	    "  -v, --version     Print version\n"
 	    "Report bugs to <cjustin@bcgsc.ca>.";
 	std::cerr << dialog << std::endl;
