87035f16a755e936823310994f86556e41559ee7
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2019-10-17 12:05:21 -0700

    Update physlr.py

--- physlr/physlr.py
+++ physlr/physlr.py
@@ -203,7 +203,7 @@ class Physlr:
                         sys.exit(1)
                     line = fin.readline()
                     if not line: # a graph with no edges
-                        print("Warning: input graph has no edges, input file:\n", 
+                        print("Warning: input graph has no edges, input file:\n",
                               filename, file=sys.stderr)
                         break
                     if Physlr.args.verbose >= 2:
