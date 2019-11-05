829e8b950f4b4af62e443e7b8789a54b50782e68
afshinfard <a.afshinfard@gmail.com>
2019-11-05 09:17:02 -0800

    added mol2-bcs and mol2-bkcs

--- physlr/physlr.py
+++ physlr/physlr.py
@@ -1841,6 +1841,8 @@ class Physlr:
             for pos, u in enumerate(path):
                 if u not in bxtomxs:
                     u = u.rsplit("_", 1)[0]
+                if u not in bxtomxs:
+                    u = u.rsplit("_", 1)[0]
                 for mx in bxtomxs[u]:
                     mxtopos.setdefault(mx, set()).add((tid, pos))
         print(
