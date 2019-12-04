8f0e331e37da6b9ee4b02217bcb5097fbcc93ac6
afshinfard <a.afshinfard@gmail.com>
2019-12-04 13:06:00 -0800

    c2-x x variable

--- physlr/physlr.py
+++ physlr/physlr.py
@@ -2171,34 +2171,31 @@ class Physlr:
 
     def physlr_subgraphs_stats(self):
         "Retrieve subgraphs' stats."
+        nodes_of_interest = []
         if len(self.args.FILES) > 1:
             gin = self.read_graph([self.args.FILES[0]])
             with open(self.args.FILES[1]) as fin:
                 for line in fin:
-                    junctions.append(line.split()[0])
+                    nodes_of_interest.append(line.split()[0])
             print(
                 int(timeit.default_timer() - t0),
-                "Separating junction-causing barcodes into molecules "
-                "using the following algorithm(s):\n\t",
-                alg_list_2d,
-                #self.args.strategy.replace("++"," / ").replace("+", " + "),
-                "\n\tand other barcodes with bc.",
+                "Computing statistics for subgraphs of interest...",
                 file=sys.stderr)
         else:
             gin = self.read_graph(self.args.FILES)
+            print(
+                int(timeit.default_timer() - t0),
+                "Computing statistics of all subgraphs...", file=sys.stderr)
+            nodes_of_interest = gin
+        #Physlr.filter_edges(gin, self.args.n)
 
-        gin = self.read_graph(self.args.FILES)
-        Physlr.filter_edges(gin, self.args.n)
-        print(
-            int(timeit.default_timer() - t0),
-            "Computing statistics of the subgraphs...", file=sys.stderr)
         if self.args.threads == 1:
             stats = dict(self.subgraph_stats(gin, u) for u in progress(gin))
         else:
             Physlr.graph = gin
             with multiprocessing.Pool(self.args.threads) as pool:
                 stats = dict(pool.map(
-                    self.subgraph_stats_process, progress(gin), chunksize=100))
+                    self.subgraph_stats_process, progress(nodes_of_interest), chunksize=100))
             Physlr.graph = None
         print(int(timeit.default_timer() - t0), "Extracted subgraphs' statistics.", file=sys.stderr)
         self.write_subgraphs_stats(stats, sys.stdout)
