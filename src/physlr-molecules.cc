4caa390203bcec39932cc5643582593a6e2a350c
Johnathan Wong <jowong@hpce704.hpc.bcgsc.ca>
2019-12-17 11:39:54 -0800

    physlr-molecules: polished biconnected component code

--- src/physlr-molecules.cc
+++ src/physlr-molecules.cc
@@ -6,7 +6,6 @@
 #include <string>
 #include <vector>
 
-#include "tsl/robin_set.h"
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/biconnected_components.hpp>
 #include <boost/graph/graph_utility.hpp>
@@ -59,7 +58,7 @@ struct edge_component_t
 	{
 		num = 555
 	};
-	typedef edge_property_tag kind;
+	using kind = edge_property_tag;
 } edge_component;
 }
 
@@ -73,6 +72,10 @@ using vertex_t = graph_t::vertex_descriptor;
 using edge_t = graph_t::edge_descriptor;
 using barcodeToIndex_t = std::unordered_map<std::string, vertex_t>;
 using indexToBarcode_t = std::unordered_map<vertex_t, std::string>;
+using vertexSet_t = std::unordered_set<vertex_t>;
+using componentToVertexSet_t = std::vector<vertexSet_t>;
+using vertexToComponent_t = std::unordered_map<vertex_t, size_t>;
+using vecVertexToComponent_t = std::vector<vertexToComponent_t>;
 
 static void
 printVersion()
@@ -137,10 +140,10 @@ main(int argc, char* argv[])
 	char* end = nullptr;
 	static int benchmark = 0;
 	static int help = 0;
-	std::string separationStrategy;
-	// bool verbose = false;
+	std::string separationStrategy = "bc";
+	bool verbose = false;
 	unsigned t = 1;
-	// bool failed = false;
+	bool failed = false;
 	static const struct option longopts[] = {
 		{ "help", no_argument, &help, 1 },
 		{ "benchmark", no_argument, &benchmark, 1 },
@@ -155,7 +158,7 @@ main(int argc, char* argv[])
 			separationStrategy.assign(optarg);
 			break;
 		case 'v':
-			// verbose = true;
+			verbose = true;
 			break;
 		case 't':
 			t = strtoul(optarg, &end, 10);
@@ -175,32 +178,29 @@ main(int argc, char* argv[])
 		exit(EXIT_SUCCESS);
 	} else if (infiles.empty()) {
 		printErrorMsg(progname, "missing file operand");
-		// failed = true;
+		failed = true;
 	}
-	t = t + 1;
+	if (t > 1) {
+		std::cerr << "physlr-molecules does not support multithreading currently." << std::endl;
+		t = 1;
+	}
+	if (separationStrategy.compare("bc")) {
+		std::cerr << "physlr-molecules only supports biconnected components currently."
+		          << std::endl;
+		separationStrategy = "bc";
+	}
 	// TODO add checks for separation strategy
-	/*if (failed) {
-	    printUsage(progname);
-	    exit(EXIT_FAILURE);
-	}*/
-
-	/*enum
-	{
-	    A,
-	    B,
-	    C,
-	    D,
-	    E,
-	    F
-	};
-*/
+	if (failed) {
+		printUsage(progname);
+		exit(EXIT_FAILURE);
+	}
 
 	graph_t g;
+	std::cerr << "Loading graph" << std::endl;
+
 #if _OPENMP
 	double sTime = omp_get_wtime();
 #endif
-	std::cerr << "Using bgl" << std::endl;
-	std::cerr << "Loading Graph" << std::endl;
 
 	barcodeToIndex_t barcodeToIndex;
 	indexToBarcode_t indexToBarcode;
@@ -217,11 +217,13 @@ main(int argc, char* argv[])
 			}
 			if (line == "" or line == "U\tV\tn") {
 				atEdges = true;
-				std::cerr << "Added vertices to graph" << std::endl;
+				if (verbose) {
+					std::cerr << "Loaded vertices to graph ";
 #if _OPENMP
-				std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
-				sTime = omp_get_wtime();
+					std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+					sTime = omp_get_wtime();
 #endif
+				}
 				continue;
 			}
 
@@ -244,143 +246,104 @@ main(int argc, char* argv[])
 			}
 		}
 	}
-	std::cerr << "Added edges to graph" << std::endl;
+	if (verbose) {
+		std::cerr << "Loaded edges to graph ";
+	} else {
+		std::cerr << "Loaded graph ";
+	}
 #if _OPENMP
 	std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
 	sTime = omp_get_wtime();
 #endif
 	std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB" << std::endl;
-	// barcodeToIndex.clear();
-	// boost::print_graph(g);
-	using vertexSet_t = std::unordered_set<vertex_t>;
-	using componentVertexSet_t = std::vector<vertexSet_t>;
+
 	uint64_t vertexNum = indexToBarcode.size();
 
-	std::vector<std::unordered_map<vertex_t, size_t>> componentsOfVertices;
-	componentsOfVertices.resize(indexToBarcode.size());
-	// double subgraphTime = 0, biconnectedTime = 0, cleanupTime = 0, componentTime = 0,
-	//       insertMapTime = 0;
-	if (benchmark != 0) {
-		if (vertexNum > 100000) {
-			vertexNum = 100000;
-		}
-		sTime = omp_get_wtime();
-	}
+	vecVertexToComponent_t vecVertexToComponent;
+	vecVertexToComponent.resize(vertexNum);
+
 #if _OPENMP
 	sTime = omp_get_wtime();
 #endif
-	double totalBiconnectedTime = 0, totalSubgraphTime = 0;
-	// auto vertexItRange = vertices(g);
+
 	for (uint64_t vertexId = 0; vertexId < vertexNum; ++vertexId) {
-		// for (auto vertexIt = vertexItRange.first; vertexIt != vertexItRange.second; ++vertexIt) {
-		// std::cout << "Index: " << *vertexIt << std::endl;
-		// auto neighbours = boost::adjacent_vertices(*vertexIt, g);
-#if _OPENMP
-		double subgraphTime = omp_get_wtime();
-#endif
+		// Find neighbour of vertex and generate neighbour induced subgraph
 		auto neighbours = boost::adjacent_vertices(vertexId, g);
-		/*graph_t& g1 = g.create_subgraph();
-		for (auto vd : make_iterator_range(neighbours))
-		    boost::add_vertex(vd, g1);
-		boost::print_graph(g1);
-		std::cout << "Index: " << *vertexIt << std::endl;*/
-		graph_t& g1 = g.create_subgraph(neighbours.first, neighbours.second);
-#if _OPENMP
-		totalSubgraphTime += (omp_get_wtime() - subgraphTime);
-#endif
-		// boost::print_graph(g1);
-#if _OPENMP
-		double biconnectedTime = omp_get_wtime();
-#endif
-		property_map<graph_t, edge_component_t>::type component = get(edge_component, g1);
-		biconnected_components(g1, component);
-		// std::cerr << "Found " << num_comps << " biconnected components.\n";
+		graph_t& subgraph = g.create_subgraph(neighbours.first, neighbours.second);
+
+		// Find biconnected components
+		property_map<graph_t, edge_component_t>::type component = get(edge_component, subgraph);
+		biconnected_components(subgraph, component);
 
 		std::vector<vertex_t> art_points_vec;
-		articulation_points(g1, std::back_inserter(art_points_vec));
+		articulation_points(subgraph, std::back_inserter(art_points_vec));
 		std::unordered_set<vertex_t> art_points(art_points_vec.begin(), art_points_vec.end());
-		// std::cerr << "Found " << art_points.size() << " articulation points.\n";
-		// for (auto&& x : art_points)
-		//	std::cerr << g1[x].indexOriginal << std::endl;
-#if _OPENMP
-		totalBiconnectedTime += (omp_get_wtime() - biconnectedTime);
-#endif
+
+		// Remove articulation points from biconnected components
 		graph_traits<graph_t>::edge_iterator ei, ei_end;
-		componentVertexSet_t componentVertices;
-		for (boost::tie(ei, ei_end) = edges(g1); ei != ei_end; ++ei) {
+		componentToVertexSet_t componentToVertexSet;
+
+		for (boost::tie(ei, ei_end) = edges(subgraph); ei != ei_end; ++ei) {
 			size_t componentNum = component[*ei];
-			if (componentNum + 1 > componentVertices.size()) {
-				componentVertices.resize(componentNum + 1);
+			if (componentNum + 1 > componentToVertexSet.size()) {
+				componentToVertexSet.resize(componentNum + 1);
 			}
-			vertex_t node1 = source(*ei, g1);
-			vertex_t node2 = target(*ei, g1);
+
+			vertex_t node1 = source(*ei, subgraph);
+			vertex_t node2 = target(*ei, subgraph);
+
 			if (art_points.find(node1) == art_points.end()) {
-				// std::cout << g1[node1].indexOriginal << " " << componentNum << std::endl;
-				componentVertices[componentNum].insert(g1[node1].indexOriginal);
+				componentToVertexSet[componentNum].insert(subgraph[node1].indexOriginal);
 			}
 			if (art_points.find(node2) == art_points.end()) {
-				// std::cout << g1[node2].indexOriginal << " " << componentNum << std::endl;
-				componentVertices[componentNum].insert(g1[node2].indexOriginal);
+				componentToVertexSet[componentNum].insert(subgraph[node2].indexOriginal);
 			}
 		}
+
 		size_t moleculeNum = 0;
-		std::unordered_map<vertex_t, size_t> vertexToComponent;
-		for (auto&& vertexSet : componentVertices) {
-			// std::cout << "size: " << vertexSet.size() << std::endl;
+		vertexToComponent_t vertexToComponent;
+
+		// Remove components with size less than 1
+		for (auto&& vertexSet : componentToVertexSet) {
 			if (vertexSet.size() <= 1) {
 				continue;
 			}
 			for (auto&& vertex : vertexSet) {
 				vertexToComponent[vertex] = moleculeNum;
-				// std::cout << vertex << std::endl;
 			}
 			moleculeNum++;
 		}
-		// std::cerr << g.m_children.size() << std::endl;
+
+		// Delete subgraph to keep memory in control
 		for (auto i = g.m_children.begin(); i != g.m_children.end(); ++i) {
 			delete *i;
 		}
 		g.m_children.clear();
-		// auto edgeItRange = edges(g1);
-		// for (auto edgeIt = edgeItRange.first; edgeIt != edgeItRange.second; ++edgeIt) {
-		//		remove_edge(*edgeIt, g1);
-		//	}
 
-		componentsOfVertices[vertexId] = vertexToComponent;
-		// std::cout << "len of component: " << component.size() << std::endl;
+		vecVertexToComponent[vertexId] = vertexToComponent;
 	}
-	std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB" << std::endl;
-	std::cerr << "Finish mol sep " << std::endl;
+
+	std::cerr << "Finished molecule separation ";
 #if _OPENMP
 	std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
 	sTime = omp_get_wtime();
 #endif
+	std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB" << std::endl;
 
-	if (benchmark != 0) {
-		std::cerr << "biconnected time: " << totalBiconnectedTime << std::endl;
-		std::cerr << "subgraph time: " << totalSubgraphTime << std::endl;
-		exit(0);
-	}
-	// printGraph(g);
+	std::cerr << "Generating new graph" << std::endl;
+
 	graph_t outG;
 	barcodeToIndex_t outGBarcodeToIndex;
-	for (size_t i = 0; i < componentsOfVertices.size(); i++) {
-		/*std::pair<vertex_t, size_t> result;
-		result = *std::max_element(
-		    componentsOfVertices[i].begin(),
-		    componentsOfVertices[i].end(),
-		    [](const std::pair<vertex_t, size_t>& p1, const std::pair<vertex_t, size_t>& p2) {
-		        return p1.second < p2.second;
-		    });*/
+	for (size_t i = 0; i < vecVertexToComponent.size(); i++) {
 
 		size_t maxVal = 0;
-		for (auto&& val : componentsOfVertices[i]) {
+		for (auto&& val : vecVertexToComponent[i]) {
 			if (val.second > maxVal) {
 				maxVal = val.second;
 			}
 		}
-		for (size_t j = 0; j < maxVal + 1; j++) {
 
+		for (size_t j = 0; j < maxVal + 1; j++) {
 			vertex_t u = add_vertex(outG);
 			outG[u].name = g[i].name + "_" + std::to_string(j);
 			outG[u].weight = g[i].weight;
@@ -388,22 +351,39 @@ main(int argc, char* argv[])
 			outGBarcodeToIndex[outG[u].name] = u;
 		}
 	}
+
 	auto edgeItRange = edges(g);
 	for (auto edgeIt = edgeItRange.first; edgeIt != edgeItRange.second; ++edgeIt) {
 		vertex_t u, v;
 		u = g[boost::source(*edgeIt, g)].indexOriginal;
 		v = g[boost::target(*edgeIt, g)].indexOriginal;
-		if (componentsOfVertices[u].find(v) == componentsOfVertices[u].end() ||
-		    componentsOfVertices[v].find(u) == componentsOfVertices[v].end()) {
+
+		if (vecVertexToComponent[u].find(v) == vecVertexToComponent[u].end() ||
+		    vecVertexToComponent[v].find(u) == vecVertexToComponent[v].end()) {
 			continue;
 		}
-		size_t uMolecule = componentsOfVertices[u][v];
-		size_t vMolecule = componentsOfVertices[v][u];
+
+		size_t uMolecule = vecVertexToComponent[u][v];
+		size_t vMolecule = vecVertexToComponent[v][u];
 		std::string uName = g[u].name + "_" + std::to_string(uMolecule);
 		std::string vName = g[v].name + "_" + std::to_string(vMolecule);
 		edge_t e = add_edge(outGBarcodeToIndex[uName], outGBarcodeToIndex[vName], outG).first;
 		outG[e].weight = g[*edgeIt].weight;
 	}
+
+	std::cerr << "Generated new graph ";
+#if _OPENMP
+	std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+	sTime = omp_get_wtime();
+#endif
+
 	std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB" << std::endl;
+
 	printGraph(outG);
+	if (verbose) {
+		std::cerr << "Printed graph" << std::endl;
+#if _OPENMP
+		std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+#endif
+	}
 }
