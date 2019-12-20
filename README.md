15340ff332e48c7dd1a1522126b83f56637dba03
Johnathan Wong <34543031+jowong4@users.noreply.github.com>
2019-12-20 13:14:55 -0800

    Physlr molecules C++ port (#129)
    * physlr-molecules: add first iteration to repo
    
    * physlr-molecules: implement bi-connected components
    
    * physlr-molecules: draft version of biconnected components with boost
    
    * physlr-molecules: polished biconnected component code
    
    * Makefile: add physlr-molecules
    
    * Makefile: add physlr-molecules to install
    
    * physlr-molecules: remove unecessary code
    
    * azure: install boost
    
    * azure: fix format
    
    * physlr-molecules: remove boost using namespace directive
    
    * physlr-molecules: fix clang-tidy errors
    
    * physlr-molecules: add no lint for delete
    
    * .clang-tidy: ignore StackAddressEscape
    
    * physlr-molecules: rename variable
    
    * physlr-molecules: improve help
    
    * physlr-molecules: rename rest of edgeComponent
    
    * physlr-molecules: clang-format
    
    * physlr-molecules: modularlize readTSV
    
    * physlr-molecules: modularlize componentToNewGraph
    
    * physlr-molecules: modularlize biconnectedComponents
    
    * physlr-molecules: rename art_points_vec
    
    * physlr-molecules: remove benchmark variable
    
    * physlr-molecules: move status message into readTSV
    
    * physlr-molecules: add unit test
    
    * unittest: convert spaces to tab
    
    * physlr-molecules: improve incorrect parameter logic
    
    * physlr-molecules: update status message
    
    * physlr-molecules: improve help
    
    * physlr-molecules: remove thread option
    
    * physlr-molecules: add comment to componentsToNewGraph
    
    * README: add boost to dependencies
    
    * physlr-molecules: improve help
    
    * physlr-molecules: improve help
    
    * physlr-molecules: improve componentsToNewGraph comment
    
    * physlr-molecules: complete biconnected components in one function call
    
    * physlr-molecules: rename art_points to artPoints
    
    * physlr-molecules: rename oldG and newG to inG and outG
    
    * physlr-molecules: rename remaning newG to molSeparatedG
    
    * physlr-molecules: rename outG to molSeparatedG
    
    * physlr-molecules: rename molSeparatedG to molSepG
    
    * physlr-molecules: declare and define variables on the same line
    
    * physlr-molecules: use auto and auto&
    
    * physlr-molecules: use for loop instead of while loop
    
    * physlr-molecules: use pre-increment instead of post-increment
    
    * physlr-molecules: remove flag variable
    
    * physlr-molecules: use uint64_t for memory return type
    
    * physlr-molecules: use INT_MAX instead of 555
    
    * physlr-molecules: fix clang-tidy
    
    * physlr-molecules: ensure stringstream extraction is sucessful
    
    * physlr-molecules: use vertexSet_t

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Physlr constructs a *de novo* physical map using linked reads from 10X Genomics 
 
 * [ntCard](https://github.com/bcgsc/ntCard)
 * [ntHits](https://github.com/bcgsc/ntHits)
-* GCC 5 or newer with OpenMP
+* GCC 5 or newer with [OpenMP](https://www.openmp.org) and [boost](https://www.boost.org)
 * Python 3.5 or newer and the following packages
     * [community](https://python-louvain.readthedocs.io/en/latest/api.html)
     * [networkx](https://networkx.github.io/)
---
+++ src/physlr-molecules.cc
@@ -0,0 +1,409 @@
+#include <fstream>
+#include <getopt.h>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/biconnected_components.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/subgraph.hpp>
+
+#if _OPENMP
+#include <omp.h>
+#endif
+
+#define PROGRAM "physlr-molecules"
+#define PACKAGE_NAME "physlr"
+#define GIT_REVISION "pre-autotools"
+
+static uint64_t
+memory_usage()
+{
+	int mem = 0;
+	std::ifstream proc("/proc/self/status");
+	for (std::string s; std::getline(proc, s);) {
+		if (s.substr(0, 6) == "VmSize") {
+			std::stringstream convert(s.substr(s.find_last_of('\t'), s.find_last_of('k') - 1));
+			if (!(convert >> mem)) {
+				return 0;
+			}
+			return mem;
+		}
+	}
+	return mem;
+}
+
+struct vertexProperties
+{
+	std::string name = "";
+	int weight = 0;
+	size_t indexOriginal = 0;
+};
+
+struct edgeProperties
+{
+	int weight = 0;
+};
+
+struct edgeComponent_t
+{
+	enum
+	{
+		num = INT_MAX
+	};
+	using kind = boost::edge_property_tag;
+} edgeComponent;
+
+using graph_t = boost::subgraph<boost::adjacency_list<
+    boost::vecS,
+    boost::vecS,
+    boost::undirectedS,
+    vertexProperties,
+    boost::property<
+        boost::edge_index_t,
+        int,
+        boost::property<edgeComponent_t, std::size_t, edgeProperties>>>>;
+using vertex_t = graph_t::vertex_descriptor;
+using edge_t = graph_t::edge_descriptor;
+using barcodeToIndex_t = std::unordered_map<std::string, vertex_t>;
+using indexToBarcode_t = std::unordered_map<vertex_t, std::string>;
+using vertexSet_t = std::unordered_set<vertex_t>;
+using componentToVertexSet_t = std::vector<vertexSet_t>;
+using vertexToComponent_t = std::unordered_map<vertex_t, size_t>;
+using vecVertexToComponent_t = std::vector<vertexToComponent_t>;
+
+static void
+printVersion()
+{
+	const char VERSION_MESSAGE[] =
+	    PROGRAM " (" PACKAGE_NAME ") " GIT_REVISION "\n"
+	            "Written by Johnathan Wong.\n"
+	            "\n"
+	            "Copyright 2019 Canada's Michael Smith Genome Science Centre\n";
+	std::cerr << VERSION_MESSAGE << std::endl;
+	exit(EXIT_SUCCESS);
+}
+
+static void
+printErrorMsg(const std::string& progname, const std::string& msg)
+{
+	std::cerr << progname << ": " << msg
+	          << "\nTry 'physlr-molecules --help' for more information.\n";
+}
+
+static void
+printUsage(const std::string& progname)
+{
+	std::cout << "Usage:  " << progname
+	          << "  [-s SEPARATION-STRATEGY] [-v] FILE...\n\n"
+	             "  -v         enable verbose output\n"
+	             "  -s --separation-strategy   \n"
+	             "  SEPARATION-STRATEGY      `+` separated list of molecule separation strategies "
+	             "[bc]\n"
+	             "  --help     display this help and exit\n";
+}
+
+void
+printGraph(const graph_t& g)
+{
+	std::cout << "U\tn" << std::endl;
+	auto vertexItRange = boost::vertices(g);
+	for (auto vertexIt = vertexItRange.first; vertexIt != vertexItRange.second; ++vertexIt) {
+		auto& node1 = g[*vertexIt].name;
+		auto& weight = g[*vertexIt].weight;
+		std::cout << node1 << "\t" << weight << "\n";
+	}
+	std::cout << "\nU\tV\tn" << std::endl;
+	auto edgeItRange = boost::edges(g);
+	for (auto edgeIt = edgeItRange.first; edgeIt != edgeItRange.second; ++edgeIt) {
+		auto& weight = g[*edgeIt].weight;
+		auto& node1 = g[boost::source(*edgeIt, g)].name;
+		auto& node2 = g[boost::target(*edgeIt, g)].name;
+		std::cout << node1 << "\t" << node2 << "\t" << weight << "\n";
+	}
+}
+
+void
+readTSV(graph_t& g, const std::vector<std::string>& infiles, bool verbose)
+{
+	auto progname = "physlr-molecules";
+	std::cerr << "Loading graph" << std::endl;
+#if _OPENMP
+	double sTime = omp_get_wtime();
+#endif
+	barcodeToIndex_t barcodeToIndex;
+	indexToBarcode_t indexToBarcode;
+	for (auto& infile : infiles) {
+		infile == "-" ? "/dev/stdin" : infile;
+		std::ifstream infileStream(infile);
+		for (std::string line; std::getline(infileStream, line);) {
+			if (line == "U\tn") {
+				continue;
+			}
+			if (line.empty()) {
+				break;
+			}
+			std::string node1;
+			int weight;
+			std::istringstream ss(line);
+			if (ss >> node1 >> weight) {
+				auto u = boost::add_vertex(g);
+				g[u].name = node1;
+				g[u].weight = weight;
+				g[u].indexOriginal = u;
+				barcodeToIndex[node1] = u;
+				indexToBarcode[u] = node1;
+			} else {
+				printErrorMsg(progname, "unknown graph format");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (verbose) {
+			std::cerr << "Loaded vertices to graph ";
+#if _OPENMP
+			std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+			sTime = omp_get_wtime();
+#endif
+		}
+		for (std::string line; std::getline(infileStream, line);) {
+			if (line == "U\tV\tn") {
+				continue;
+			}
+			if (line.empty()) {
+				printErrorMsg(progname, "unknown graph format");
+				exit(EXIT_FAILURE);
+			}
+			std::string node1, node2;
+			int weight;
+			std::istringstream ss(line);
+			if (ss >> node1 >> node2 >> weight) {
+				auto E = boost::add_edge(barcodeToIndex[node1], barcodeToIndex[node2], g).first;
+				g[E].weight = weight;
+			} else {
+				printErrorMsg(progname, "unknown graph format");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (verbose) {
+			std::cerr << "Loaded edges to graph ";
+		} else {
+			std::cerr << "Loaded graph ";
+		}
+#if _OPENMP
+		std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+		sTime = omp_get_wtime();
+#endif
+		std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB"
+		          << std::endl;
+	}
+}
+
+/* Generate a molecule separated graph (molSepG) using component/community information from
+molecule separation (vecVertexToComponent). The input graph (inG) is the barcode overlap graph
+or a molecule separated graph from the previous round of molecule separation.*/
+void
+componentsToNewGraph(
+    const graph_t& inG,
+    graph_t& molSepG,
+    vecVertexToComponent_t& vecVertexToComponent)
+{
+	barcodeToIndex_t molSepGBarcodeToIndex;
+#if _OPENMP
+	double sTime = omp_get_wtime();
+#endif
+	for (size_t i = 0; i < vecVertexToComponent.size(); ++i) {
+
+		size_t maxVal = 0;
+		for (auto&& val : vecVertexToComponent[i]) {
+			if (val.second > maxVal) {
+				maxVal = val.second;
+			}
+		}
+
+		for (size_t j = 0; j < maxVal + 1; ++j) {
+			auto u = boost::add_vertex(molSepG);
+			molSepG[u].name = inG[i].name + "_" + std::to_string(j);
+			molSepG[u].weight = inG[i].weight;
+			molSepG[u].indexOriginal = u;
+			molSepGBarcodeToIndex[molSepG[u].name] = u;
+		}
+	}
+
+	auto edgeItRange = boost::edges(inG);
+	for (auto edgeIt = edgeItRange.first; edgeIt != edgeItRange.second; ++edgeIt) {
+		auto& u = inG[boost::source(*edgeIt, inG)].indexOriginal;
+		auto& v = inG[boost::target(*edgeIt, inG)].indexOriginal;
+
+		if (vecVertexToComponent[u].find(v) == vecVertexToComponent[u].end() ||
+		    vecVertexToComponent[v].find(u) == vecVertexToComponent[v].end()) {
+			continue;
+		}
+
+		auto& uMolecule = vecVertexToComponent[u][v];
+		auto& vMolecule = vecVertexToComponent[v][u];
+		auto uName = inG[u].name + "_" + std::to_string(uMolecule);
+		auto vName = inG[v].name + "_" + std::to_string(vMolecule);
+		auto e =
+		    boost::add_edge(molSepGBarcodeToIndex[uName], molSepGBarcodeToIndex[vName], molSepG)
+		        .first;
+		molSepG[e].weight = inG[*edgeIt].weight;
+	}
+
+	std::cerr << "Generated new graph ";
+#if _OPENMP
+	std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+	sTime = omp_get_wtime();
+#endif
+
+	std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB" << std::endl;
+}
+
+void
+biconnectedComponents(graph_t& subgraph, vertexToComponent_t& vertexToComponent)
+{
+	// Find biconnected components
+	boost::property_map<graph_t, edgeComponent_t>::type component =
+	    boost::get(edgeComponent, subgraph);
+
+	std::vector<vertex_t> artPointsVec;
+	boost::biconnected_components(subgraph, component, std::back_inserter(artPointsVec));
+
+	vertexSet_t artPoints(artPointsVec.begin(), artPointsVec.end());
+
+	// Remove articulation points from biconnected components
+	boost::graph_traits<graph_t>::edge_iterator ei, ei_end;
+	componentToVertexSet_t componentToVertexSet;
+
+	for (boost::tie(ei, ei_end) = boost::edges(subgraph); ei != ei_end; ++ei) {
+		size_t componentNum = component[*ei];
+		if (componentNum + 1 > componentToVertexSet.size()) {
+			componentToVertexSet.resize(componentNum + 1);
+		}
+
+		auto node1 = source(*ei, subgraph);
+		auto node2 = target(*ei, subgraph);
+
+		if (artPoints.find(node1) == artPoints.end()) {
+			componentToVertexSet[componentNum].insert(subgraph[node1].indexOriginal);
+		}
+		if (artPoints.find(node2) == artPoints.end()) {
+			componentToVertexSet[componentNum].insert(subgraph[node2].indexOriginal);
+		}
+	}
+
+	size_t moleculeNum = 0;
+
+	// Remove components with size less than 1
+	for (auto&& vertexSet : componentToVertexSet) {
+		if (vertexSet.size() <= 1) {
+			continue;
+		}
+		for (auto&& vertex : vertexSet) {
+			vertexToComponent[vertex] = moleculeNum;
+		}
+		++moleculeNum;
+	}
+}
+
+int
+main(int argc, char* argv[])
+{
+
+	auto progname = "physlr-molecules";
+	int optindex = 0;
+	static int help = 0;
+	std::string separationStrategy = "bc";
+	bool verbose = false;
+	bool failed = false;
+	static const struct option longopts[] = {
+		{ "help", no_argument, &help, 1 },
+		{ "separation-strategy", required_argument, nullptr, 's' },
+		{ nullptr, 0, nullptr, 0 }
+	};
+	for (int c; (c = getopt_long(argc, argv, "s:v", longopts, &optindex)) != -1;) {
+		switch (c) {
+		case 0:
+			break;
+		case 's':
+			separationStrategy.assign(optarg);
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		default:
+			exit(EXIT_FAILURE);
+		}
+	}
+	std::vector<std::string> infiles(&argv[optind], &argv[argc]);
+	if (argc < 1) {
+		failed = true;
+	}
+	if (help != 0) {
+		printVersion();
+		printUsage(progname);
+		exit(EXIT_SUCCESS);
+	} else if (infiles.empty()) {
+		printErrorMsg(progname, "missing file operand");
+		failed = true;
+	}
+	if (separationStrategy != "bc") {
+		printErrorMsg(progname, "unsupported molecule separation strategy");
+		failed = true;
+	}
+	if (failed) {
+		printUsage(progname);
+		exit(EXIT_FAILURE);
+	}
+
+	graph_t g;
+	readTSV(g, infiles, verbose);
+
+	vecVertexToComponent_t vecVertexToComponent;
+	vecVertexToComponent.resize(boost::num_vertices(g));
+
+#if _OPENMP
+	double sTime = omp_get_wtime();
+#endif
+	auto vertexItRange = vertices(g);
+	for (auto vertexIt = vertexItRange.first; vertexIt != vertexItRange.second; ++vertexIt) {
+		// Find neighbour of vertex and generate neighbour induced subgraph
+		auto neighbours = boost::adjacent_vertices(*vertexIt, g);
+		graph_t& subgraph = g.create_subgraph(neighbours.first, neighbours.second);
+
+		vertexToComponent_t vertexToComponent;
+		biconnectedComponents(subgraph, vertexToComponent);
+
+		// Delete subgraph to keep memory in control
+		for (auto& i : g.m_children) {
+			// NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
+			delete i;
+		}
+		g.m_children.clear();
+
+		vecVertexToComponent[*vertexIt] = vertexToComponent;
+	}
+
+	std::cerr << "Finished molecule separation ";
+#if _OPENMP
+	std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+	sTime = omp_get_wtime();
+#endif
+	std::cerr << "Memory usage: " << double(memory_usage()) / double(1048576) << "GB" << std::endl;
+
+	std::cerr << "Generating molecule overlap graph" << std::endl;
+
+	graph_t molSepG;
+	componentsToNewGraph(g, molSepG, vecVertexToComponent);
+	printGraph(molSepG);
+	if (verbose) {
+		std::cerr << "Printed graph" << std::endl;
+#if _OPENMP
+		std::cerr << "in sec: " << omp_get_wtime() - sTime << std::endl;
+#endif
+	}
+}
