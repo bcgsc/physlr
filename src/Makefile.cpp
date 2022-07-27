463567b1efb95082997b006f9027ed107cf8b394
afshinfard <a.afshinfard@gmail.com>
2022-07-26 22:55:21 -0700

    makebf using btllib

---
+++ src/Makefile.cpp
@@ -0,0 +1,20 @@
+all: physlr-makebf
+
+CXXFLAGS+=-O3 -Wall -Werror -Wno-unknown-pragmas -std=c++11 -pthread -fopenmp # See if you can exclude fopenmp
+LDFLAGS+=-l btllib
+
+physlr-makebf: physlr-makebf.cpp
+	$(CXX) -o $@ $< $(CXXFLAGS) $(LDFLAGS)
+
+# Check the C++ source code for errors.
+lint: clang-format clang-tidy
+
+# Check the C++ source code for errors with clang-tidy.
+clang-tidy:
+	clang-tidy -warnings-as-errors='*' *.cpp -- -std=c++11 -x c++ -Ibtllib/include -Iinclude $(CXXFLAGS) 
+
+# Check the C++ source code for white-space errors with clang-format.
+clang-format:
+	for i in *.cpp; do clang-format -style=file $$i >$$i.fixed; done
+	for i in *.cpp; do diff -su $$i $$i.fixed && rm -f $$i.fixed; done
+	if ls *.fixed; then exit 1; fi
