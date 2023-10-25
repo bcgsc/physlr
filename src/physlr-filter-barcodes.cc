a9081c834b646305135e8b3857a7b71ae091a4da
afshinfard <a.afshinfard@gmail.com>
2023-10-25 16:02:25 -0700

    test errors

--- src/physlr-filter-barcodes.cc
+++ src/physlr-filter-barcodes.cc
@@ -53,10 +53,8 @@ printUsage(const std::string& progname)
 }
 
 using Mx = uint64_t;
-using MxVal = uint64_t;
-using MxPos = std::size_t;
-using Mxs = tsl::robin_set<MxVal>;
-using MxwithPos = std::pair<MxVal, MxPos>;
+using Mxs = tsl::robin_set<Mx>;
+using MxwithPos = std::pair<Mx, std::size_t>;
 using MxswithPos = tsl::robin_set<MxwithPos>;
 using Bx = std::string;
 using BxtoMxs = tsl::robin_map<Bx, MxswithPos>;
@@ -78,7 +76,6 @@ readMxs(std::istream& is, const std::string& ipath, bool silent, bool positioned
     bool hasPos = false;
 	while ((is >> bx) && (getline(is, mx_line))) {
 		std::istringstream iss(mx_line);
-        // if the file does not contain minimizer positions, then add -1 as position
 		if (positioned) {
             while (iss >> mx) {
                 size_t mxsep = mx.find(":");
@@ -90,7 +87,7 @@ readMxs(std::istream& is, const std::string& ipath, bool silent, bool positioned
                 }
                 mxpos = mx.substr(mxsep+1);
                 mx = mx.substr(0, mxsep);
-                bxtomxs[bx].insert( std::pair<MxVal, MxPos>(strtoull(mx.c_str(), nullptr, 0), strtoull(mxpos.c_str(), nullptr, 0)) );
+                bxtomxs[bx].insert(std::make_pair(strtoull(mx.c_str(), nullptr, 0), strtoull(mxpos.c_str(), nullptr, 0)));
             }
         } else { 
             while (iss >> mx) {
@@ -106,7 +103,7 @@ readMxs(std::istream& is, const std::string& ipath, bool silent, bool positioned
                 } else if (hasPos) {
                     mx = mx.substr(0, mx.find(":"));
                 }
-                bxtomxs[bx].insert(std::pair<MxVal, MxPos>(strtoull(mx.c_str(), nullptr, 0), -1));
+                bxtomxs[bx].insert(std::make_pair(strtoull(mx.c_str(), nullptr, 0), 0));
             }
         }
 	}
@@ -175,21 +172,21 @@ removeSingletonMxs(BxtoMxs& bxtomxs, bool silent)
 	uint64_t singletons = 0;
 	tsl::robin_map<Mx, bool> counted;
 	for (const auto& item : bxtomxs) {
-		MxswithPos not_singletons;
-        //std::unique_ptr<MxswithPos> not_singletons;
-		//not_singletons->reserve(item.second.size());
-		not_singletons.reserve(item.second.size());
+		//MxswithPos not_singletons;
+        std::unique_ptr<MxswithPos> not_singletons;
+		not_singletons->reserve(item.second.size());
+		//not_singletons.reserve(item.second.size());
 		for (const auto& mx : item.second) {
 			if (counts[mx.first] >= 2) {
-				//not_singletons->insert({mx.first, mx.second});
-				not_singletons.insert({mx.first, mx.second});
+				not_singletons->insert({mx.first, mx.second});
+				//not_singletons.insert({mx.first, mx.second});
 			} else {
 				++singletons;
 			}
 		}
 		uniqueMxs.insert(item.second.begin(), item.second.end()); // need to do before removing!
-		//bxtomxs[item.first] = std::move(*not_singletons);
-		bxtomxs[item.first] = std::move(not_singletons);
+		bxtomxs[item.first] = std::move(*not_singletons);
+		//bxtomxs[item.first] = std::move(not_singletons);
 	}
 	auto t = std::chrono::steady_clock::now();
 	auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(t - t0);
